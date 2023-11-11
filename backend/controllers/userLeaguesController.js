b89157b59c3c83b421ab7b7ebca774f9e40c375c
jmiarons <joelmiarons@protonmail.com>
2023-11-11 22:15:23 +0100

    Added invite via SMS

--- backend/controllers/userLeaguesController.js
+++ backend/controllers/userLeaguesController.js
@@ -1,38 +1,75 @@
-const { json } = require("express");
+const { json, raw} = require("express");
 const db = require("../db.js");
+const e = require("express");
+const auth = Buffer.from('u5f50a388c5c1dcd45117cd910dc05587:17E9826F479772C0E7A1687C47DD9387').toString("base64");
+require('dotenv').config()
+const autToken = process.env.AUTHTOKEN
+const auth = Buffer.from(autToken).toString("base64");
+
+
 const UserLeagueController = () => {
 
-
-    const joinLeague = async (req, res) => {
+    const inviteLeague = async (req, res) => {
         const userMail = req.get('token');
         const leagueId = req.body.league_id;
-        
-        // check if user and league exist
+        const phoneNumber = req.body.phone_number;
+
+        let invitedMail = await getMailByPhone(phoneNumber)
+
+        // checks
         try {
+            const userLeagueOwner = await db.query('SELECT * FROM leagues WHERE creator_mail=$1', [userMail]);
+            if (userLeagueOwner.rows.length === 0) {
+                return res.status(403).json({success: false, message: 'User not owns the league'})
+            }
+
+            const userLeagueExists = await db.query('SELECT * FROM usersleagues WHERE league_id=$1 AND user_mail=$2', [leagueId, invitedMail])
+            if (userLeagueExists.rows.length !== 0) {
+                return res.status(409).json({success: false, message: 'User already in this league'});
+            }
+
+            const phoneNumberNotExists = await db.query('SELECT * FROM users WHERE phone_number=$1', [phoneNumber])
+            if (phoneNumberNotExists.rows.length === 0) {
+                return res.status(404).json({success: false, message: 'Phone number not found'})
+            }
+
             const leagueResponse = await db.query('SELECT * FROM leagues WHERE league_id=$1', [leagueId]);
             if (leagueResponse.rows.length === 0) {
-                return res.status(404).json({success:false, message:'League not found'});
+                return res.status(404).json({success: false, message: 'League not found'});
             }
+
             const userResponse = await db.query('SELECT * FROM users WHERE user_mail=$1', [userMail]);
             if (userResponse.rows.length === 0) {
-                return res.status(404).json({success:false, message:'User not found'});
+                return res.status(404).json({success: false, message: 'User not found'});
             }
-            
+
+        }
+        catch (err) {
+            console.log(err)
+            return res.status(500).json({success:false, message:'Internal Server Error'});
+        }
+
+        let leagueName = await getLeagueName(leagueId)
+        let username = await getUsername(userMail)
+
+        let message = 'You have been invited to ' + leagueName + ' by ' + username + '.'
+        // TODO afegir el link de la view de my leagues
+        await sendMessage(phoneNumber, message)
+
+        // core functionality
+        try {
+            const result = await db.query('INSERT INTO usersleagues (user_mail, league_id) VALUES($1, $2);', [invitedMail, leagueId]);
+
+
+            return res.status(200).json({ success: true });
+
         }
         catch (err) {
             console.error(err);
             return res.status(500).json({success:false, message:'Internal Server Error'});
         }
+    }
 
-        // main core functionality
-        try {
-            const result = await db.query('INSERT INTO usersleagues (user_mail, league_id) VALUES($1, $2);', [userMail, leagueId]);
-            return res.status(200).json({ success: true });
-        } catch (err) {
-            console.error(err);
-            return res.status(500).json({success:false, message:'Internal Server Error'});
-        }
-    };
     const leaveLeague = async (req, res) => {
         const userMail = req.get('token');
         const leagueId = req.body.league_id;
@@ -67,8 +104,45 @@ const UserLeagueController = () => {
         }
 
     };
+
+    const getLeagueName = async (leagueId) => {
+        let result = await db.query('SELECT league_name FROM leagues WHERE league_id = $1', [leagueId])
+        return result.rows[0].league_name
+    }
+
+    const getUsername = async (userMail) => {
+        let result = await db.query('SELECT username FROM users WHERE user_mail = $1', [userMail])
+        return result.rows[0].username
+    }
+
+    const getMailByPhone = async (phoneNumber) => {
+        let result = await db.query('SELECT user_mail FROM users WHERE phone_number = $1', [phoneNumber])
+        return result.rows[0].user_mail
+    }
+
+    const sendMessage = async (phoneNumber, message) => {
+        let data = {
+            from:    "StepBro",
+            to:      phoneNumber,
+            message: message
+        }
+        data = new URLSearchParams(data);
+        data = data.toString();
+
+
+        await fetch("https://api.46elks.com/a1/sms", {
+            method: "post",
+            body: data,
+            headers: {"Authorization": "Basic "  + Buffer.from('u5f50a388c5c1dcd45117cd910dc05587:17E9826F479772C0E7A1687C47DD9387').toString("base64")}
+        })
+            .then(res => res.json())
+            .then(json => console.log(json))
+            .catch(err => console.log(err))
+
+    }
+
     return {
-        joinLeague,
+        inviteLeague,
         leaveLeague,
     };
     
