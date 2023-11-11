f29e52c93adcfa2e90a8ae5b1c9424219c04c0ee
EncryptEx <limusina10@gmail.com>
2023-11-11 07:06:10 +0100

    set up express

---
+++ backend/routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+// Define routes
+router.get('/users', (req, res) => {
+  res.send('List of users');
+});
+
+router.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`Details of user ${userId}`);
+});
+
+router.post('/users', (req, res) => {
+  res.send('Create a new user');
+});
+
+router.put('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`Update user ${userId}`);
+});
+
+router.delete('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`Delete user ${userId}`);
+});
+
+module.exports = router;
