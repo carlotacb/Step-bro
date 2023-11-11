c586536e970af9ae5afc0a53553e13eced200adb
EncryptEx <limusina10@gmail.com>
2023-11-11 11:47:36 +0100

    Refactor database query function to use arrow function syntax.

--- backend/db.js
+++ backend/db.js
@@ -6,20 +6,6 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 });
 
-exports.query = async function (q) {
-  const client = await pool.connect()
-  let res
-  try {
-    await client.query('BEGIN')
-    try {
-      res = await client.query(q);
-      await client.query('COMMIT');
-    } catch (err) {
-      await client.query('ROLLBACK');
-      throw err
-    }
-  } finally {
-    client.release();
-  }
-  return res;
+module.exports = {
+    query: (text, params) => pool.query(text, params)
 };
