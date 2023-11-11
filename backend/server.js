7a92a0aa33c7331816821a1cb75c8bcd2c0dbdc3
jmiarons <joelmiarons@protonmail.com>
2023-11-11 16:34:48 +0100

    <3

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   next();
+// });
+
+
 require('dotenv').config()
 
 const swaggerUI = require('swagger-ui-express');
