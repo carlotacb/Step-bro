9b293fac62fa3f0f2a6006ed4fcb759afbc3103b
EncryptEx <limusina10@gmail.com>
2023-11-11 08:13:03 +0100

    working db connector backend

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
+require('dotenv').config()
 
 const { Pool } = require('pg');
 
+
+
 const pool = new Pool({
-  user: 'your_username',
-  password: 'your_password',
-  host: 'localhost',
-  port: 5432, // default Postgres port
-  database: 'your_database_name'
+  user: process.env.DB_USER,
+  password: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT, // default Postgres port
+  database: process.env.DB_NAME
 });
 
 module.exports = {
   query: (text, params) => pool.query(text, params)
 };
+
+
+
+const port = process.env.PORT;
 
 
 const routes = require('./routes');
