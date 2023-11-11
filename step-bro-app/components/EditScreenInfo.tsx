422d385fca39bea795547f920c10c71bbeadc2f0
adria.martinez.mogro <adria.martinez.mogro@estudiantat.upc.edu>
2023-11-11 17:34:29 +0200

    Home pero falla circular-progress-indicator

--- step-bro-app/components/EditScreenInfo.tsx
+++ step-bro-app/components/EditScreenInfo.tsx
@@ -6,7 +6,6 @@ import { ExternalLink } from './ExternalLink';
 import { MonoText } from './StyledText';
 import { Text, View } from './Themed';
 
-
 export default function EditScreenInfo({ path }: { path: string }) {
   return (
     <View>
