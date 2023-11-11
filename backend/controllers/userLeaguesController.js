b9226fcd20c675d64ae17db658d9c06860c381ec
EncryptEx <limusina10@gmail.com>
2023-11-11 15:05:41 +0100

    Add userLeaguesController and routes for joining and leaving leagues

---
+++ backend/controllers/userLeaguesController.js
@@ -0,0 +1,78 @@
+const { json } = require("express");
+const db = require("../db.js");
+const UserLeagueController = () => {
+
+
+    const joinLeague = async (req, res) => {
+        const userMail = req.body.user_mail;
+        const leagueId = req.body.league_id;
+        
+        // check if user and league exist
+        try {
+            const leagueResponse = await db.query('SELECT * FROM leagues WHERE league_id=$1', [leagueId]);
+            if (leagueResponse.rows.length === 0) {
+                return res.status(404).json({success:false, message:'League not found'});
+            }
+            const userResponse = await db.query('SELECT * FROM users WHERE user_mail=$1', [userMail]);
+            if (userResponse.rows.length === 0) {
+                return res.status(404).json({success:false, message:'User not found'});
+            }
+            
+        }
+        catch (err) {
+            console.error(err);
+            return res.status(500).json({success:false, message:'Internal Server Error'});
+        }
+
+        // main core functionality
+        try {
+            const result = await db.query('INSERT INTO usersleagues (user_mail, league_id) VALUES($1, $2);', [userMail, leagueId]);
+            return res.status(200).json({ success: true });
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json({success:false, message:'Internal Server Error'});
+        }
+    };
+    const leaveLeague = async (req, res) => {
+        const userMail = req.body.user_mail;
+        const leagueId = req.body.league_id;
+
+        // check if user and league exist and is in league
+        try {
+            const leagueResponse = await db.query('SELECT * FROM leagues WHERE league_id=$1', [leagueId]);
+            if (leagueResponse.rows.length === 0) {
+                return res.status(404).json({success:false, message:'League not found'});
+            }
+            const userResponse = await db.query('SELECT * FROM users WHERE user_mail=$1', [userMail]);
+            if (userResponse.rows.length === 0) {
+                return res.status(404).json({success:false, message:'User not found'});
+            }
+            const ownResponse = await db.query('SELECT * FROM usersleagues WHERE user_mail=$1 AND league_id=$2', [userMail, leagueId]);
+            if (ownResponse.rows.length === 0) {
+                return res.status(403).json({success:false, message:'You are not in this league'});
+            }
+            
+        }
+        catch (err) {
+            console.error(err);
+            return res.status(500).json({success:false, message:'Internal Server Error'});
+        }
+        // do core functionality
+        try {   
+            const result = await db.query('DELETE FROM usersleagues WHERE user_mail = $1 AND league_id = $2;', [userMail, leagueId]);
+            return res.status(200).json({ success: true });
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json('Internal Server Error');
+        }
+
+    };
+    return {
+        joinLeague,
+        leaveLeague,
+    };
+    
+}
+module.exports = {UserLeagueController};
+
+
--- backend/routes.js
+++ backend/routes.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const {UserController} = require('./controllers/userController');
 const {AuthController} = require('./controllers/authController');
 const {LeagueController} = require('./controllers/leagueController');
+const {UserLeagueController} = require('./controllers/userLeaguesController');
 
 const userController = UserController();
 const authController = AuthController();
 const leagueController = LeagueController();
+const userLeagueController = UserLeagueController();
 
 // Auth routes
 router.post('/login', authController.login);
@@ -26,6 +28,10 @@ router.post('/leagues', leagueController.createLeague);
 router.get('/leagues/:creator_mail&:league_name', leagueController.getLeagueById);
 router.put('/leagues/:creator_mail&:league_name', leagueController.updateLeague);
 router.delete('/leagues/:creator_mail&:league_name', leagueController.deleteLeague);
+
+// UserLeagues routes
+router.post('/userleague', userLeagueController.joinLeague);
+router.delete('/userleague', userLeagueController.leaveLeague);
 
 
 module.exports = router;
